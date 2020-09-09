97fe236622955eb92bb5e8e92842200ea35ffe61
dheeraj <ndheerajreddy18@gmail.com>
2020-09-09 23:58:11 +0530

    basic mode got over

--- question.js
+++ question.js
@@ -7,9 +7,74 @@ var questions=[{
    "answer":"2"
 },{
    "question":"which country is the worst hit by covid-19 till now?",
+   "option1":"china",
+   "option2":"USA",
+   "option3":"india",
+   "option4":"brazil",
+   "answer":"2"
+
+},{
+   "question":"In which country  covid-19 is bielieved to be originated ?",
    "option1":"india",
    "option2":"china",
    "option3":"USA",
    "option4":"brazil",
-   "answer":"3"
+   "answer":"2"
+},{
+   "question":"which country produced the first covid-19 vaccine?",
+   "option1":"india",
+   "option2":"england",
+   "option3":"germany",
+   "option4":"russia",
+   "answer":"4"
+
+
+},{
+   "question":"which activity is most likely to expose you to risk of corona virus?",
+   "option1":"picking groceries",
+   "option2":"visiting a salon",
+   "option3":"attending dinners",
+   "option4":"being in crowds",
+   "answer":"4"
+
+},{
+   "question":"U are at risk if u have been in contact with covid patenient in the last?",
+   "option1":"30 mins",
+   "option2":"5 mins",
+   "option3":"one day",
+   "option4":"all",
+   "answer":"4"
+
+},{
+   "question":"which therapy is recommendedto treat covid-19 by ICMR ?",
+   "option1":"gene therapy",
+   "option2":"plasma therapy",
+   "option3":"hydro therapy",
+   "option4":"occupational therapy",
+   "answer":"2"
+
+},{
+   "question":"most effective way to protect yourself from others is by maintaining?",
+   "option1":"3 foot dis",
+   "option2":"1 foot dis",
+   "option3":"o.5 foot dis",
+   "option4":"no dis at all",
+   "answer":"1"
+},{
+   "question":"Incubation and recovery period for covid-19 infections?",
+   "option1":"2 weeks for mild cases",
+   "option2":"3-6 weeks for severe",
+   "option3":"1 week for sympton onset",
+   "option4":"all",
+   "answer":"4"
+},{
+   "question":"The first inidan case of covid-19 pandemic was reported in ?",
+   "option1":"tamil nadu",
+   "option2":"maharastra",
+   "option3":"andhra pradesh",
+   "option4":"bihar",
+   "answer":"1"
+
+
+  
 }]
--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 var currentquestion=0;
 var score=0;
 var totalquestions=questions.length;
+var cq=0;
+var answers=[];
 
 var container=document.getElementById("quiz");
 var questionEL=document.getElementById("question");
@@ -24,21 +26,80 @@ function loadquestion(questionindex){
 
 };
 
+function check(){
+    var optioncheck=document.querySelector('input[type=radio]:checked');
+    var a=optioncheck.value;
+    var r=document.getElementsByClassName("options");
+      
+
+      for (var i=0;i<4;i++)
+      {
+         if(r[i].checked==false)
+          r[i].disabled=true;
+      }
+    if(questions[currentquestion].answer==a)
+    {
+      
+      document.body.style.backgroundColor="limegreen";
+      
+      setTimeout(function(){
+        alert("CORRECT");
+      },500);
+    
+    }
+    else
+    { 
+        document.body.style.backgroundColor="red";
+        setTimeout(function(){
+          alert("WRONG");
+        },250);
+    }
+}
 
 function loadnextquestion(){
-
+   
+    document.body.style.backgroundColor="skyblue";
+    var r=document.getElementsByClassName("options");
+    var c=currentquestion+1;
+    var q=answers[c];
+    for (var i=0;i<4;i++)
+    {
+       
+        
+        r[i].disabled=false;
+    }
+    if(q>0)
+    {
+      
+      
+      r[q-1].checked=true;
+      for (var i=0;i<4;i++)
+      {
+        if(r[i].checked==false)
+            r[i].disabled=true;
+      }       
+    
+    }
+   else
+   { 
     var selectedoption=document.querySelector('input[type=radio]:checked');
     if(!selectedoption){
         alert("select ur answer");
         return;
-    }
+     }
      var answer=selectedoption.value;
+     answers.push(answer);
      if(questions[currentquestion].answer==answer)
     {
+        
         score+=1;
+
     }
     selectedoption.checked=false;
-    currentquestion++;
+  }  
+    ++currentquestion;
+    cq++;
+    
     if(currentquestion==totalquestions-1)
     {
         nextbutton.textContent="submit";
@@ -51,7 +112,41 @@ function loadnextquestion(){
         resultcont.textContent="your score:"+score;
         return;
     }
+   
+    
+
     loadquestion(currentquestion);
+  
+}
+
+function loadpreviousquestion(){
+   currentquestion--;
+   loadquestion(currentquestion);
+   document.body.style.backgroundColor="skyblue";
+   var r=document.getElementsByClassName("options");
+   var q=answers[currentquestion];
+  for (var i=0;i<4;i++)
+  {
+     if(r[i].checked==false)
+      
+      r[i].disabled=true;
+  }
+  
+  r[q-1].disabled=false;
+   if(r[q-1].checked!=true)   
+   {
+   
+    r[q-1].checked=true;
+   }
+  if(question==totalquestions-1)
+    {
+        nextbutton.textContent="submit";
+
+    }
+    else
+       nextbutton.textContent="Next question";
+
+
 
 }
 loadquestion(currentquestion);
